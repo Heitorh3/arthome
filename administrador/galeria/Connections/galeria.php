3e065194b5257aef0c605448ce07180c2a0d884e
Heitorh3 <heitorh3@gmail.com>
2013-10-14 09:38:50 -0300

    Update galeria.php

--- administrador/galeria/Connections/galeria.php
+++ administrador/galeria/Connections/galeria.php
@@ -3,7 +3,7 @@
 # Type="MYSQL"
 # HTTP="true"
 $hostname_galeria = "localhost";
-$database_galeria = "galeria";
+$database_galeria = "db_arthome";
 $username_galeria = "root";
 $password_galeria = "floresta";
 $galeria = mysql_pconnect($hostname_galeria, $username_galeria, $password_galeria) or trigger_error(mysql_error(),E_USER_ERROR); 
