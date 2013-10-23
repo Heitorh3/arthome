0dfaf33c1f1f0231a6f82cd12c20025eacd1b775
Heitor A Neto <heitorh3@gmail.com>
2013-10-23 15:27:43 -0300

    Correcoes

--- administrador/cadastra_imagens.php
+++ administrador/cadastra_imagens.php
@@ -1,8 +1,24 @@
 <?php include('verifica.php'); if ($_SESSION['tipo'] == 'Administrador') { ?>
+<?php   
+        // Conexão com o MySQL
+        // ========================
+        include('../classes/conexao.php');
+
+        $conexao = new Conexao();
+        $conexao->criaConexao();
+        // ====(Fim da conexão)====
+
+        // Monta a consulta MySQL  que fará a busca do slide show
+        $sql_albums = "SELECT id, title FROM `tbl_albums`;";
+        // Executa a consulta
+        $query = mysql_query($sql_albums);  
+                
+        $conexao->fechaConexao();                   
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Arthome - Upload</title>
 <script type="text/javascript" src="galeria/js/swfobject.js"></script>
 <script type="text/javascript" src="galeria/js/multiUpload.js"></script>
@@ -33,11 +49,14 @@
                 <div id="headend"><b>Cadastrar Imagens</b> <br /> <p>Espaço reservado para o cadastro de imagens de novos produtos!</p></div>
                 <div id="inscricao">                                                       
                     <div id="uploader"></div>
-                    <div >
-                        <select style="margin: 15px 0px 0px 20px; width: 90px; height:25px;" >
-                          <option value="volvo">Volvo</option>
+                    <div align="left">
+                        Selecione um Album
+                        <select name="album_id" id="album_id" style="margin: 15px 0px 0px 20px; width: 190px; height:25px;">
+                            <?php while ($row_albums = mysql_fetch_assoc($query)){ ?>                
+                                <option value=<?php echo $row_albums['id']; ?>><?php echo $row_albums['title']; ?></option>
+                            <?php } ?>
                         </select>
-                    </div>
+                    </div></br>
                      <div id="uploader_files"></div>
                     <div style="margin: 10px 0 0 60px; width: auto"> 
                         <input class="botao" id="acao" name="acao" type="submit" onclick="javascript:uploader.startUpload();"value="Inserir" /> &nbsp; 
