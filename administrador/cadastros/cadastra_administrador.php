c0fe09fb97b73108fe8f2ac4a347c6d67670a89a
Heitor A Neto <heitorh3@gmail.com>
2013-10-23 08:42:01 -0300

    Página de erro padrão

--- administrador/cadastros/cadastra_administrador.php
+++ administrador/cadastros/cadastra_administrador.php
@@ -18,14 +18,12 @@
 		$conexao->criaConexao();
 		
 		$consulta = mysql_query($sql);
-		if(! $consulta) {
-			echo 'ERRO: '.mysql_error();
-			echo '<br /> Número: '.mysql_errno();
-		}
-		
-		$conexao->fechaConexao();
-		
-		header('Location: ../sucesso.php?t=adm');
+
+		if (!$consulta){	
+		header('Location: ../erro.php?t=error&msg='.mysql_errno());		
+		}else {
+			header('Location: ../sucesso.php?t=adm');
+		}		
 	}
 
 ?>
--- administrador/cadastros/cadastrar_novidades.php
+++ administrador/cadastros/cadastrar_novidades.php
@@ -15,15 +15,12 @@
 	
 		$conexao->criaConexao();
 		
-		$consulta = mysql_query($sql);
-		if(! $consulta) {
-			echo 'ERRO: '.mysql_error();
-			echo '<br /> Número: '.mysql_errno(); 
-		}
-		
-		$conexao->fechaConexao();
-		
-		header('Location: ../sucesso.php?t=not');
+		if (!$consulta){	
+		header('Location: ../erro.php?t=error&msg='.mysql_errno());		
+		}else {
+			header('Location: ../sucesso.php?t=not');
+		}			
+		$conexao->fechaConexao();		
 	}
 
 ?>
